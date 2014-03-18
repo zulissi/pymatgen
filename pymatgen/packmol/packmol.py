7d7d6908e930717acfcfcdb04f14856e6f5f05d4
Yong Zhang <yzhang19@nd.edu>
2014-03-18 17:29:34 -0400

    made get_auto_boxsize global
    Former-commit-id: 0b00af6f648121c326f81e7f01abfe232d9fc23e [formerly 14fc08bcf9c6b5c94813a8131688ee137a246c4c]
    Former-commit-id: ab8b1e51a8fa4a59ba45114350eee0c806c750b5

--- pymatgen/packmol/packmol.py
+++ pymatgen/packmol/packmol.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import tempfile
-from monty.io import ScratchDir
-
+from monty.tempfile import ScratchDir
 from pymatgen import Molecule
 from pymatgen.io.babelio import BabelMolAdaptor
 import pybel as pb
@@ -35,38 +34,30 @@ class PackmolRunner(object):
         else:
             return some_obj
 
-    def _get_auto_boxsize(self):
-        """
-        TODO: Put docs here!!
-        :param idx:
-        :raise NotImplementedError:
-        """
 
-        volume=0.0
-        for idx,mol in enumerate(self.mols):
-#        for mol in self.mols
-#        mol=self.mols[idx]
-#        print mol[0]
-#        print self.mols[idx][0]
-#        lx=max(mol[i].coords[0] for i,atom in enumerate(mol))-min(mol[i].coords[0] for i,atom in enumerate(mol))
-#        ly=max(mol[i].coords[1] for i,atom in enumerate(mol))-min(mol[i].coords[1] for i,atom in enumerate(mol))
-#        lz=max(mol[i].coords[2] for i,atom in enumerate(mol))-min(mol[i].coords[2] for i,atom in enumerate(mol))
-            lx,ly,lz = np.max(mol.cart_coords,0)-np.min(mol.cart_coords,0)
-            print lx,ly,lz
-            lx += 2.0
-            ly += 2.0
-            lz += 2.0
-            length=max(lx,ly,lz)
-            print length
-#            length=length*int(self.param_list[idx]['number'])
-            volume += length**(3.0)*float(self.param_list[idx]['number'])
-        length = volume**(1.0/3.0)
-        print length
+#    def _get_auto_boxsize(self):
+#        """
+#        TODO: Put docs here!!
+#        :param idx:
+#        :raise NotImplementedError:
+#        """
+#
+#        volume=0.0
+#        for idx,mol in enumerate(self.mols):
+#            lx,ly,lz = np.max(mol.cart_coords,0)-np.min(mol.cart_coords,0)
+##            print lx,ly,lz
+#            lx += 2.0  # assume tolerance = 2.0
+#            ly += 2.0
+#            lz += 2.0
+#            length=max(lx,ly,lz)
+#            volume += length**(3.0)*float(self.param_list[idx]['number'])
+#        length = volume**(1.0/3.0)
+##        print length
+#
+#        for idx,mol in enumerate(self.mols):
+#            self.param_list[idx]['inside box']='0.0 0.0 0.0 {} {} {}'.format(length,length,length)
+##        raise NotImplementedError('Auto box size is not implemented yet!')
 
-        for idx,mol in enumerate(self.mols):
-            self.param_list[idx]['inside box']='0.0 0.0 0.0 {} {} {}'.format(length,length,length)
-#        print self.param_list[idx]['inside box']
-#        raise NotImplementedError('Auto box size is not implemented yet!')
 
     def run(self):
         """
@@ -76,7 +67,7 @@ class PackmolRunner(object):
 
         scratch = tempfile.gettempdir()
         with ScratchDir(scratch,copy_to_current_on_exit=True) as d:
-#        with ScratchDir(scratch) as d:
+        #with ScratchDir(scratch) as d:
             # convert mols to pdb files
             for idx, mol in enumerate(self.mols):
                 a = BabelMolAdaptor(mol)
@@ -85,8 +76,9 @@ class PackmolRunner(object):
 
             # TODO: also check if user specified outside box, etc.
             # Do not use auto mode if user specified any type of box
-            if 'inside box' not in self.param_list[idx]:
-                self._get_auto_boxsize()
+
+            # ignore user input for now, always calculate box size
+            #self._get_auto_boxsize()
 
             with open(os.path.join(d, 'pack.inp'), 'w') as inp:
                 # create packmol control file
@@ -97,18 +89,43 @@ class PackmolRunner(object):
                     inp.write('\n')
                     inp.write('structure {}.pdb\n'.format(os.path.join(d, str(idx))))
 
+                    #ignore box size from user input for now
+                    boxlength = get_auto_boxsize(mol,self.param_list[idx]['number'])
+                    #print boxlength
+                    self.param_list[idx]['inside box']='0.0 0.0 0.0 {} {} {}'.format(boxlength,boxlength,boxlength)
                     for k, v in self.param_list[idx].iteritems():
                         inp.write('  {} {}\n'.format(k, self._format_packmol_str(v)))
                     inp.write('end structure\n')
 
             proc = Popen(['packmol'], stdin=open(os.path.join(d, 'pack.inp'), 'r'),stdout=PIPE)
             (stdout, stderr) = proc.communicate()
-            print stdout
-            print stderr
+#            print stdout
+#            print stderr
 
 
             a = BabelMolAdaptor.from_file(os.path.join(d, "box.pdb"), "pdb")
             return a.pymatgen_mol
+
+def get_auto_boxsize(mol,number):
+    """
+    TODO: Put docs here!!
+    :param mol:
+    :raise NotImplementedError:
+    """
+
+    #print number
+    volume=0.0
+    lx,ly,lz = np.max(mol.cart_coords,0)-np.min(mol.cart_coords,0)
+    #print lx,ly,lz
+    lx += 2.0  # assume tolerance = 2.0
+    ly += 2.0
+    lz += 2.0
+    length=max(lx,ly,lz)
+    volume += length**(3.0)*float(number)
+    length = volume**(1.0/3.0)
+    #print length
+    return length
+
 
 if __name__ == '__main__':
     coords = [[0.000000, 0.000000, 0.000000],
@@ -120,4 +137,6 @@ if __name__ == '__main__':
 #    pmr = PackmolRunner([mol, mol], [{"number":4,"inside box":[0.,0.,0.,40.,40.,40.]}, {"number":5, "inside box":[0.,0.,0.,40.,40.,40.]}])
     pmr = PackmolRunner([mol, mol], [{"number":4,"inside box":[0.,0.,0.,40.,40.,40.]}, {"number":5}])
     s = pmr.run()
-    print s
+#    print s
+
+

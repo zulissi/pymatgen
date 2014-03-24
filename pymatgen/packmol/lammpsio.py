eba64ed84bb1ea053780ee6a42ed958d6f8d31db
Yong Zhang <yzhang19@nd.edu>
2014-03-23 22:49:51 -0400

    lammps log file parser
    Former-commit-id: 7b0791e31412a916d46e9265bda916a1b0d5c14a [formerly b49d692af89667cbdcc27a42c81f1d34f49f2c41]
    Former-commit-id: 3998694957cad3bef6efa1e909a82f146e4cffcd

--- pymatgen/packmol/lammpsio.py
+++ pymatgen/packmol/lammpsio.py
@@ -1,33 +1,27 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Dec 22 22:40:29 2013
-
-@author: asharma6
-"""
+#!/usr/bin/env python
 
 import re
 import numpy as np
 
 
 class LammpsLog:
-    """Parser for LAMMPS log file (parse function). Saves the output properties (log file) in the form of a dictionary (LOG) with the key being
+    """
+    Parser for LAMMPS log file (parse function).
+    Saves the output properties (log file) in the form of a dictionary (LOG) with the key being
     the LAMMPS output property (see 'thermo_style custom' command in the LAMMPS documentation).
     For example, LOG['temp'] will return the temperature data array in the log file.
     """
-    def __init__(self, filename, *properties):
+    def __init__(self, filename):
         """
         Args:
             filename:
                 Filename of the LAMMPS logfile.
-            properties:
-                Properties you want to be evaluated. Right now, pymatgen only supports viscosity (argument: viscosity) evaluation.
         """
         
         self.filename=filename
-        self.properties=properties
-        #print properties
         self.LOG={} # Dictionary LOG has all the output property data as numpy 1D arrays with the property name as the key
         self.header=0
+        self.footer_blank_line=0   # blank lines in footer
 #    @staticmethod
     def list2float(self,seq):
         for x in seq:
@@ -36,60 +30,62 @@ class LammpsLog:
           except ValueError:
             yield x
             
-    def autocorrelate (a):
-        b=np.concatenate((a,np.zeros(len(a))),axis=1)
-        c= np.fft.ifft(np.fft.fft(b)*np.conjugate(np.fft.fft(b))).real
-        d=c[:len(c)/2]
-        d=d/(np.array(range(len(a)))+1)[::-1]
-        return d
 
-        
     def parselog(self):
         """
-        Parses the log file. rajectory file will be added 
-                later with the addition of properties based on trajectory analysis.
+        Parses the log file. 
         """
-        minimization = 0 #To avoid reading the minimization data steps
-        
+        md = 0 #To avoid reading the minimization data steps
+
         with open (self.filename,'r') as logfile:
             self.total_lines=len(logfile.readlines())
-        logfile.close 
-        logfile=open(self.filename,'r')
-        for line in logfile:
-            if 'viscosity' in self.properties:
-                cut=re.search('variable\s+viscosity_cut\s+equal\s+([0-9]+)', line)
-                if cut: self.cutoff = float(cut.group(1))
-                sep=re.search('variable viscosity_separation equal ([0-9]+)', line)
-                if sep: self.separation = float(sep.group(1))
-                win=re.search('variable viscosity_window equal ([0-9]+)', line)
-                if win: self.window = float(win.group(1))
-                t=re.search('variable T equal ([0-9]+)', line)
-                if t: self.temp = float(t.group(1))
-            
-            run=re.search('variable\s+run\s+equal\s+([0-9]+)', line)
-            if run: self.runlength = float(run.group(1))
-            step=re.search('variable timestep equal ([0-9]+)', line)
-            if step: self.timestep=float(step.group(1))
-            format = re.search('thermo_style.+', line)
-            if format: 
-              data_format = format.group().split()[2:] 
-              total_properties=len(data_format)
-            if ''.join(line.split()).lower()=='run${run}': minimization=1
-            if all(isinstance(x,float) for x in list(self.list2float(line.split()))) and minimization==1 and len(line.split())==total_properties: break
-            self.header=self.header+1
-        if 'viscosity' in self.properties:  
-            self.nl0= self.cutoff/self.separation+1 
-            self.wline=self.window/self.separation+1 #no. of lines of the window
-            self.nline=self.runlength/self.separation+1 
+        logfile.close
+
+        with open (self.filename,'r') as logfile:
+
+            for line in logfile:
+
+                # total steps of MD
+                steps = re.search('run\s+([0-9]+)',line)
+                if steps:
+                    self.md_step = float(steps.group(1))
+                    md = 1
+
+                # save freq to log
+                thermo = re.search('thermo\s+([0-9]+)',line)
+                if thermo:
+                    self.log_save_freq = float(thermo.group(1))
+
+                # log format
+                format = re.search('thermo_style.+',line)
+                if format:
+                    data_format = format.group().split()[2:]
+
+                if all(isinstance(x,float) for x in list(self.list2float(line.split()))) and md==1 : break
+                self.header=self.header+1
+
+            for line in logfile:
+                if (line == '\n'): self.footer_blank_line = self.footer_blank_line + 1
+
+            #print self.total_lines
+            #print self.md_step
+            #print self.log_save_freq
+            #print self.footer_blank_line
+
         
-        rawdata=np.genfromtxt(fname=self.filename, dtype=float, skip_header=int(self.header+self.nl0), skip_footer=int(self.total_lines-(self.header+self.nline)))
+        rawdata=np.genfromtxt(fname=self.filename, dtype=float, skip_header=int(self.header), skip_footer=int(self.total_lines-(self.header + self.md_step/self.log_save_freq + 1) - self.footer_blank_line))
+        print rawdata
         
         for column, property in enumerate (data_format):
             self.LOG[property]= rawdata[:,column]        
         
 
 
-    
+if __name__ == '__main__':
+    filename = 'log.test'
+    log = LammpsLog(filename)
+    log.parselog()
+    #print log.LOG
                     
         
         
